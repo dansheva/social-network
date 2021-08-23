e92832336fa0a4577f2c1d22e5522ef40bc952dd
dansheva <dansheva2@gmail.com>
2021-08-23 09:58:20 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header () {
+    return(
+        <header className={"header"}>
+            <img src="https://logos-download.com/wp-content/uploads/2017/07/Apple_Logo_1998.png" alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Profile () {
+    return(
+        <div className={"main-content"}>
+            <div className="big-image-container">
+                <img
+                    className="big-image full-width-image"
+                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                    alt="img"/>
+            </div>
+            <div className={"avatar-description-container"}>
+                <div className={"avatar-container"}>
+                    <img
+                        className="full-width-image"
+                        src="https://www.pngkey.com/png/full/204-2049354_ic-account-box-48px-profile-picture-icon-square.png"
+                        alt="avatar"/>
+                </div>
+                <div className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </div>
+            </div>
+            <div className="posts-container">
+                <div className="posts__title">
+                    My posts
+                </div>
+                <div className="posts__add-new-post">
+                    New post
+                </div>
+                <div className="posts__my-posts">
+                    <div className="post">
+                        Post 1
+                    </div>
+                    <div className="post">
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
