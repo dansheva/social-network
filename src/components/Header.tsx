eaa44a6eda91d19f68fe5fec372780d22823106e
dansheva <dansheva2@gmail.com>
2021-08-25 10:04:45 +0200

    tried css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 function Header () {
     return(
-        <header className={"header box-shadow"}>
-            <img className={"header__image"} src="https://logos-download.com/wp-content/uploads/2017/07/Apple_Logo_1998.png" alt="logo"/>
+        <header className={classes.header + " box-shadow"}>
+            <img className={classes.header__image} src="https://logos-download.com/wp-content/uploads/2017/07/Apple_Logo_1998.png" alt="logo"/>
         </header>
     );
 }
