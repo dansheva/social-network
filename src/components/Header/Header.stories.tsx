7703d0a82766e662190a4bb1caa8ae9c569db4cd
dansheva <dansheva2@gmail.com>
2021-09-14 10:51:58 +0200

    tried storybook

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import '../../App.css';
+
+export default {
+    title: 'Header',
+    component: Header,
+}
+
+export const NormalHeader = () => {
+    return (
+        <Header />
+    )
+}
